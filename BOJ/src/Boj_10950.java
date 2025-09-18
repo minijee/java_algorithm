736c55ae2878359de60dde19f8b7bb17f67d6ad3
jimini <jimin5305@naver.com>
2025-09-18 20:04:04 +0900

    BOJ_10950_A+B - 3

---
+++ BOJ/src/Boj_10950.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Boj_10950 {
+    /**
+     * A+B - 3 (B5)
+     */
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      int T = sc.nextInt();
+      
+      for(int t = 1; t <= T; t++) {
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        System.out.println(A + B);
+      }
+      
+    }
+}
