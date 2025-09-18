1a40e471dd4002159094c6b6883802be5806522d
jimini <jimin5305@naver.com>
2025-09-18 20:10:49 +0900

    BOJ_25083_새싹

---
+++ BOJ/src/Boj_25083.java
@@ -0,0 +1,13 @@
+public class Boj_25083 {
+	/**
+	 * 새싹 (B5)
+	 */
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
