366a46b568a175b3936fecbc1486bec52b8adc45
jimini <jimin5305@naver.com>
2025-09-18 20:09:20 +0900

    BOJ_11654_아스키 코드

---
+++ BOJ/src/Boj_11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Boj_11654 {
+	/**
+	 * 아스키 코드 (B5)
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.next().toCharArray()[0];
+		
+		System.out.println((int) c);
+	}
+}
