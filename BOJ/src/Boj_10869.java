f2d544633f5215113c91514d8fcd88c173163ff1
jimini <jimin5305@naver.com>
2025-09-18 20:02:24 +0900

    BOJ_10869_사칙연산

---
+++ BOJ/src/Boj_10869.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Boj_10869 {
+    /**
+     * 사칙연산 (B5)
+     */
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      
+      System.out.println(A + B);
+      System.out.println(A - B);
+      System.out.println(A * B);
+      System.out.println(A / B);
+      System.out.println(A % B);
+    }
+}
