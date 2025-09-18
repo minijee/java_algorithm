3b213a82468f2139998cf9b8d266e4cc2537f017
jimini <jimin5305@naver.com>
2025-09-18 20:05:51 +0900

    BOJ_10951_A+B - 4

---
+++ BOJ/src/Boj_10951.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Boj_10951 {
+	/**
+	 * A+B - 4 (B5)
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println(A + B);
+		}
+	}
+}
